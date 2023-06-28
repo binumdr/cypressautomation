2e30c4e4fef5027878cb8929b31ca3d8dbcc2177
rupesh <rupesh.manandhar@resimator.fi>
2023-06-28 12:08:47 +0545

    login automation code
    Signed-off-by: rupesh <rupesh.manandhar@resimator.fi>

---
+++ cypress/e2e/User CRUD/userwithemptyfield.cy.js
@@ -0,0 +1,20 @@
+describe('MyTestSuite', function()
+{
+    it('loginpage', function()
+    {
+        cy.visit('https://gold.noveltytechnology.com/login')
+        cy.wait(300)
+        cy.get('#username').type('tester@noveltytechnology.com')
+        cy.wait(300)
+        cy.get('#password').type('P@ssw0rd@2023')
+        cy.wait(300)
+        cy.get('#root > div.MuiBox-root.css-1b3jhod > div.MuiBox-root.css-18b97fs > div > form > div.MuiBox-root.css-xuhmcu').click()
+        cy.wait(3000)
+        cy.get('.css-kprzq2').click().wait(5000)
+        cy.reload().wait(5000)
+        cy.get('body').click()
+        cy.get('.css-69i1ev > .MuiStack-root > .MuiButtonBase-root').click().wait(2000)
+        cy.get('.MuiDialogActions-root.css-1bsw83b > .MuiButtonBase-root.css-1q39k4i').click()
+    })
+
+})
