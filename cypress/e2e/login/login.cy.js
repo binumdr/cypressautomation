782fb66e51962fd713e74058c1f392235a08bc0d
rupesh <rupesh.manandhar@resimator.fi>
2023-06-28 11:50:19 +0545

    login automation by cypress

---
+++ cypress/e2e/login/login.cy.js
@@ -0,0 +1,15 @@
+describe('MyTestSuite', function()
+{
+    it('loginpage', function()
+    {
+        cy.visit('https://gold.noveltytechnology.com/login')
+        cy.wait(3000)
+        cy.get('#username').type('tester@noveltytechnology.com')
+        cy.wait(3000)
+        cy.get('#password').type('P@ssw0rd@2023')
+        cy.wait(3000)
+        cy.get('#root > div.MuiBox-root.css-1b3jhod > div.MuiBox-root.css-18b97fs > div > form > div.MuiBox-root.css-xuhmcu').click()
+        cy.wait(3000)
+    })
+
+})
