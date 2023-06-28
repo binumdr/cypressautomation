740ed94225e6fb399cc2f76b8845c2e1961efae8
rupesh <rupesh.manandhar@resimator.fi>
2023-06-28 15:06:48 +0545

    name check

--- cypress/e2e/User CRUD/userwithemptyfield.cy.js
+++ cypress/e2e/User CRUD/userwithemptyfield.cy.js
@@ -2,6 +2,7 @@ describe('MyTestSuite', function()
 {
     it('usercreatewithallfieldempty', function()
     {
+
         cy.visit('https://gold.noveltytechnology.com/login')
         cy.wait(300)
         cy.get('#username').type('tester@noveltytechnology.com')
@@ -10,6 +11,8 @@ describe('MyTestSuite', function()
         cy.wait(300)
         cy.get('#root > div.MuiBox-root.css-1b3jhod > div.MuiBox-root.css-18b97fs > div > form > div.MuiBox-root.css-xuhmcu').click()
         cy.wait(3000)
+
+        //user with empty field create 
         cy.get('.css-kprzq2').click().wait(5000)
         cy.reload().wait(5000)
         cy.get('body').click()
